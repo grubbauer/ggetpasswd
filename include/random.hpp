5b17e0ef70887365501be0e6817ea877e30c03c9
Raphael G. Grubbauer <raphaelgrubbauer@proton.me>
2025-08-14 19:51:05 +0200

    Added random module.

---
+++ include/random.hpp
@@ -0,0 +1,15 @@
+/* ------------------------------------
+ * File:      random.cpp
+ * Path:      include/
+ * Purpose:   Random number generation header
+ *
+ * Author:    Raphael G. Grubbauer
+ * Created:   2025-08-14
+ * Licence:   ?
+ * Copyright: (c) 2025 Raphael G. Grubbauer
+ */
+
+
+namespace grandom {
+int number(int min, int max);
+}
---
+++ src/random.cpp
@@ -0,0 +1,24 @@
+/* ------------------------------------
+ * File:      main.cpp
+ * Path:      src/
+ * Purpose:   Random number generation
+ *
+ * Author:    Raphael G. Grubbauer
+ * Created:   2025-08-14
+ * Licence:   ?
+ * Copyright: (c) 2025 Raphael G. Grubbauer
+ */
+
+#include "random.hpp"
+
+#include <random>
+
+namespace grandom {
+int number(int min, int max) {
+  std::random_device dev;
+  std::mt19937 rng(dev());
+  std::uniform_int_distribution<std::mt19937::result_type> dist(min, max);
+
+  return dist(rng);
+}
+}
